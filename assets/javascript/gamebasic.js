1a693deb64847791e7e004c21f2e1b14eb889a85
AJ <ashleyannjorgensen@gmail.com>
2018-05-19 09:11:01 -0700

    last commit

--- assets/javascript/gamebasic.js
+++ assets/javascript/gamebasic.js
@@ -1,47 +1,41 @@
 // variables 
-var alphabet = 'ABCDEF';
-var right_answer = alphabet[ parseInt(Math.random()*alphabet.length) ];
+var alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var right_answer = alphabet[parseInt(Math.random() * alphabet.length)];
 var wins = 0;
 var losses = 0;
 var guessesLeft = 7;
 var guessesSoFar = "";
 
-document.onkeyup= myFunction(event) {
-    
-}
+// document.onkeyup= myFunction(event) {  
+// }
 
 // This variable is defining when the user makes a guess- what's happening when a user inputs a letter.
-var make_guess = function()
-{
-    var userTyped = document.getElementById('guess').value;
-    document.getElementById('guess').value = '';
+var make_guess = function () {
+    var userTyped = document.getElementById("guess").value;
+    document.getElementById("guess").value = '';
     guessesSoFar = guessesSoFar + userTyped;
 
-    if (userTyped == right_answer)
-    {
+    if (userTyped == right_answer) {
         wins = wins + 1;
         // reseting the game after the user got the right answer
         guessesSoFar = "";
         guessesLeft = 7;
-        right_answer = alphabet[ parseInt(Math.random()*alphabet.length) ];
+        right_answer = alphabet[parseInt(Math.random() * alphabet.length)];
     }
-    else
-    {
+    else {
         guessesLeft = guessesLeft - 1;
         // if your guesses left get down to 0 then the game is over then restart
-        if (guessesLeft == 0 )
-        {
+        if (guessesLeft == 0) {
             losses = losses + 1;
             guessesSoFar = "";
             guessesLeft = 7;
-            right_answer = alphabet[ parseInt(Math.random()*alphabet.length) ];
+            right_answer = alphabet[parseInt(Math.random() * alphabet.length)];
         }
 
     }
     // documenting what we did in the function and if/else
-    document.getElementById('guessesLeft').innerHTML = guessesLeft;
-    document.getElementById('guessesSoFar').innerHTML = guessesSoFar;
-    document.getElementById('wins').innerHTML = wins;
-    document.getElementById('losses').innerHTML = losses;
+    document.getElementById("guessesLeft").innerHTML = guessesLeft;
+    document.getElementById("guessesSoFar").innerHTML = guessesSoFar;
+    document.getElementById("wins").innerHTML = wins;
+    document.getElementById("losses").innerHTML = losses;
 }
-   
